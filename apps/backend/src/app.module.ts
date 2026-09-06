1f983585509ca256a60026035eb62864757abbfc
heinricov abraham <heinricoov@gmail.com>
2026-09-06 09:48:11 +0700

    update: created setup endpoint

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './db/prisma.module';
 import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, TeamsModule, RolesModule],
   controllers: [],
   providers: [],
 })
